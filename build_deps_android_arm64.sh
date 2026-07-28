67bafd220d989113935248ba6ff8a1c3280c3199
MX0905 <15767433755@139.com>
2026-07-28 23:52:38 +0800

    Update build_deps_android_arm64.sh

--- build_deps_android_arm64.sh
+++ build_deps_android_arm64.sh
@@ -214,13 +214,15 @@ clone "https://code.videolan.org/videolan/x264.git" x264
 cd "$SRC_DIR/x264"
 make distclean 2>/dev/null || true
 
+CC="$NDK/toolchains/llvm/prebuilt/windows-x86_64/bin/aarch64-linux-android24-clang" \
+AR="$NDK/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ar" \
+STRIP="$NDK/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-strip" \
 ./configure \
   --host=aarch64-linux-android \
   --disable-cli \
   --enable-static \
-  --disable-shared \
+  --enable-pic \
   --prefix="$PREFIX" \
-  --cross-prefix="$NDK/toolchains/llvm/prebuilt/windows-x86_64/bin/aarch64-linux-android24-" \
   --sysroot="$NDK/toolchains/llvm/prebuilt/windows-x86_64/sysroot"
 
 make -j"$JOBS"
